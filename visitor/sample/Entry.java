07a26227c2be53d3e5920fbf0c761a0334b8318a
naokato <Naoya@okamotohitomi-no-MacBook-Air.local>
2023-01-17 22:19:35 +0900

    add visitor sample

---
+++ visitor/sample/Entry.java
@@ -0,0 +1,9 @@
+public abstract class Entry implements Element {
+  public abstract String getName();
+  public abstract int getSize();
+
+  @Override
+  public String toString() {
+    return getName() + " (" + getSize() + ")";
+  }
+}
